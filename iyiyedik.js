0b411b6af63ad41367cbac898ea088481ac51dd5
Yigit Guneli <gro.tiiy@yiit.org>
2015-02-08 18:50:10 +0200

    Update iyiyedik.js

--- iyiyedik.js
+++ iyiyedik.js
@@ -1,14 +1,29 @@
+function findVenueinMessage(message){
+  var venue_test1 = /<strong style="color:#cf1414;font-size:14px">([^<>]*)<\/strong>/;
+  var venue = venue_test1.exec(message);
+  
+  if (venue == null){
+    venue = "Other";
+  }
+  else{
+    venue = venue[1];
+  }
+
+  return venue;
+}
+
+
 function findTotalinMessage(message) {
   var value = 0;
-  var test1 = /Genel Toplam:<\/strong><\/td><td align="right" style="text-align:right"><strong>([0-9]*\,?[0-9]*) TL<\/strong>/;
-  var test2 = /Toplam<strong> :  [\s\S]* (?:<\/strong>)?[\s\S]([0-9]*\,?[0-9]*) TL/;
-  
-  var amount = test1.exec(message)
+  var value_test1 = /Genel Toplam:<\/strong><\/td><td align="right" style="text-align:right"><strong>([0-9]*\,?[0-9]*) TL<\/strong>/;
+  var value_test2 = /Toplam<strong> :  [\s\S]* (?:<\/strong>)?[\s\S]([0-9]*\,?[0-9]*) TL/;
+
+  var amount = value_test1.exec(message)
   if (amount != null){
     value = parseFloat(amount[1].replace(',','.'));
   }
   else{
-    amount = test2.exec(message);
+    amount = value_test2.exec(message);
     if (amount != null){
       value = parseFloat(amount[1].replace(',','.'));
     }
@@ -16,12 +31,15 @@ function findTotalinMessage(message) {
   return value;
 }
 
- function newChart(range, sheet, type) {
+ function newChart(range, sheet, type, position) {
    var chartBuilder = sheet.newChart();
+   
+   var x = position*20 + 1;
    chartBuilder.addRange(sheet.getRange(range))
        .setChartType(type)
-       .setOption('title', 'Spend Graph');
-   sheet.insertChart(chartBuilder.setPosition(1, 6, 0, 0).build());
+       .setOption('title', 'Spend Graph')
+       .setOption('sliceVisibilityThreshold', 0.01);
+   sheet.insertChart(chartBuilder.setPosition(x, 7, 0, 0).build());
  }
 
 function yearChanged(year){
@@ -30,7 +48,8 @@ function yearChanged(year){
   ozet.appendRow([year+":", "=SUM('"+year+"'!B1:B1048576)"])
   
   var sheet = spreadsheet.getSheetByName(String(year));
-  newChart("A:B", sheet, Charts.ChartType.LINE);
+  newChart("A:B", sheet, Charts.ChartType.LINE, 0);
+  newChart("D:E", sheet, Charts.ChartType.PIE, 1);
   
 }
   
@@ -54,7 +73,7 @@ function get_or_init_spreadsheet(){
     var ozet = spreadsheet.getActiveSheet();
     ozet.setName('ozet');
     ozet.appendRow(["","","Toplam:","=sum(B2:B1048576)"]);
-    newChart("A:B", ozet, Charts.ChartType.BAR);
+    newChart("A:B", ozet, Charts.ChartType.BAR, 0);
     ozet.autoResizeColumn(1);
     
   } else {
@@ -66,23 +85,49 @@ function get_or_init_spreadsheet(){
 
 }
 
+function removeTrigger(triggerId) {
+
+  // Loop over all triggers.
+  var allTriggers = ScriptApp.getProjectTriggers();
+  for (var i = 0; i < allTriggers.length; i++) {
+    var t = allTriggers[i].getUniqueId();
+    if (t == triggerId){
+      ScriptApp.deleteTrigger(allTriggers[i]);
+      break;
+    }
+  }
+}
+
+function removeAllTriggers() {
+
+  // Loop over all triggers.
+  var allTriggers = ScriptApp.getProjectTriggers();
+  for (var i = 0; i < allTriggers.length; i++) {
+    var t = allTriggers[i].getUniqueId();
+    Utilities.sleep(10000);
+    ScriptApp.deleteTrigger(allTriggers[i]);
+  }
+}
+
+
 function getOrderConfirmations() {
   var total = parseFloat(get_or_init_cache('total'));
   var current = parseInt(get_or_init_cache('current'));
   var messages = [];
   var cache = CacheService.getUserCache();
   var spreadsheet = get_or_init_spreadsheet();
-  var current_year = 0;
+  var current_year = get_or_init_cache('current_year');
   var threads = GmailApp.search('subject: "yemeksepeti sipariş onay "  from:yemeksepeti.com');
-
+  
   for (var i = current; i < threads.length; i++) {
     var message = threads[i].getMessages()[0];
     var message_body = message.getBody();
     var amount = findTotalinMessage(message_body);
+    var venue = findVenueinMessage(message_body);
     if (isNaN(amount)){
-      amount = 0
+      amount = 0;
     }
-    total += amount
+    total += amount;
     
     year = message.getDate().getYear();
     
@@ -94,22 +139,39 @@ function getOrderConfirmations() {
       yearChanged(year);
       current_year = year;
     }
-
-    sheet.appendRow([message.getDate(), amount, total, message.getDate().getMonth(), message.getDate().getDay()]);
+    var percentage = "=COUNTIF(D1:B10000,D"+ (sheet.getLastRow() + 1) +")/COUNTUNIQUE(D1:D10000)"
+    sheet.appendRow([message.getDate(), amount, total, venue, percentage]);
     //Utilities.sleep(500);
     cache.put("current", i);
     cache.put("total", total);
+    cache.put("current_year", current_year);
+    
+    /*removeTrigger(cache.get('trigger_id'));
+
+    var trigger = ScriptApp.newTrigger('getOrderConfirmations')
+      .timeBased()
+      .after(30000)
+      .create();
+    var trigger_id = trigger.getUniqueId();
+    cache.put('trigger_id', trigger_id);
+    */
   }
   Logger.log(total);
+  
+  var spreadsheet_url = cache.get("spreadsheet_url");
   resetCache();
-  return total;
+  return "Total spent: " +total + "<a href='" + spreadsheet_url + "'>View report</a>";
 } 
+
+function doPost(eventInfo){
+  return HtmlService.createHtmlOutput(getOrderConfirmations());
+}
 
 function doGet() {
   //return resetCache();
   //return viewCache();
-  
-  return HtmlService.createHtmlOutput("Total Spent: " + getOrderConfirmations());
+ // return HtmlService.createHtmlOutputFromFile('index.html');
+  return HtmlService.createHtmlOutput(getOrderConfirmations());
 }
 
 
@@ -121,6 +183,7 @@ function resetCache(){
   cache.remove("current");
   cache.remove("total");
   cache.remove("spreadsheet_url");
+  cache.remove("current_year");
   
   cache.put("current", 0);
   cache.put("total", 0);
